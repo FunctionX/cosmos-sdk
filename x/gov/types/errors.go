fe9673d7397f85f22b2117ac185acaa175752cd8
fx0x55 <dennis@wokoworks.com>
2022-04-12 18:56:33 +0800

    fix: register initial amount too low error

--- x/gov/types/errors.go
+++ x/gov/types/errors.go
@@ -14,4 +14,5 @@ var (
 	ErrInvalidVote             = sdkerrors.Register(ModuleName, 7, "invalid vote option")
 	ErrInvalidGenesis          = sdkerrors.Register(ModuleName, 8, "invalid genesis state")
 	ErrNoProposalHandlerExists = sdkerrors.Register(ModuleName, 9, "no handler exists for proposal type")
+	ErrInitialAmountTooLow     = sdkerrors.Register(ModuleName, 10, "initial amount too low")
 )
