4ab4bdf3ffa7fc574110cf60f5c883345669b27e
zakir <nash@wokoworks.com>
2022-05-13 16:15:25 +0800

    feat: key output add eip55 address

--- crypto/keyring/output.go
+++ crypto/keyring/output.go
@@ -1,9 +1,11 @@
 package keyring
 
 import (
+	"encoding/hex"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"golang.org/x/crypto/sha3"
 )
 
 // TODO: Move this file to client/keys
@@ -12,12 +14,13 @@ import (
 // KeyOutput defines a structure wrapping around an Info object used for output
 // functionality.
 type KeyOutput struct {
-	Name     string `json:"name" yaml:"name"`
-	Type     string `json:"type" yaml:"type"`
-	Address  string `json:"address" yaml:"address"`
-	PubKey   string `json:"pubkey" yaml:"pubkey"`
-	Mnemonic string `json:"mnemonic,omitempty" yaml:"mnemonic,omitempty"`
-	Algo     string `json:"algo" yaml:"algo"`
+	Name         string `json:"name" yaml:"name"`
+	Type         string `json:"type" yaml:"type"`
+	Eip55Address string `json:"eip55_address,omitempty" yaml:"eip55_address,omitempty"`
+	Address      string `json:"address" yaml:"address"`
+	PubKey       string `json:"pubkey" yaml:"pubkey"`
+	Mnemonic     string `json:"mnemonic,omitempty" yaml:"mnemonic,omitempty"`
+	Algo         string `json:"algo" yaml:"algo"`
 }
 
 // NewKeyOutput creates a default KeyOutput instance without Mnemonic, Threshold and PubKeys
@@ -31,12 +34,36 @@ func NewKeyOutput(keyInfo Info, a sdk.Address) (KeyOutput, error) { // nolint:in
 		return KeyOutput{}, err
 	}
 	return KeyOutput{
-		Name:    keyInfo.GetName(),
-		Type:    keyInfo.GetType().String(),
-		Address: a.String(),
-		PubKey:  string(bz),
-		Algo:    string(keyInfo.GetAlgo()),
+		Name:         keyInfo.GetName(),
+		Type:         keyInfo.GetType().String(),
+		Eip55Address: string(checksumHex(a.Bytes())),
+		Address:      a.String(),
+		PubKey:       string(bz),
+		Algo:         string(keyInfo.GetAlgo()),
 	}, nil
+}
+
+func checksumHex(addr []byte) []byte {
+	var buf = make([]byte, len(addr)*2)
+	hex.Encode(buf, addr)
+	buf = append([]byte("0x"), buf...)
+
+	// compute checksum
+	sha := sha3.NewLegacyKeccak256()
+	sha.Write(buf[2:])
+	hash := sha.Sum(nil)
+	for i := 2; i < len(buf); i++ {
+		hashByte := hash[(i-2)/2]
+		if i%2 == 0 {
+			hashByte = hashByte >> 4
+		} else {
+			hashByte &= 0xf
+		}
+		if buf[i] > '9' && hashByte > 7 {
+			buf[i] -= 32
+		}
+	}
+	return buf[:]
 }
 
 // MkConsKeyOutput create a KeyOutput in with "cons" Bech32 prefixes.
